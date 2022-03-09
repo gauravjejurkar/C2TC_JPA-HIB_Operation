3faa2b3a9696b4fc45828273572be19cd4ac2c41
my pc <my pc@G0115>
2022-03-09 19:16:20 +0530

    first

---
+++ Basic_JPA_CRUDE/src/com/gaurav/entity/Main.java
@@ -0,0 +1,38 @@
+package com.gaurav.entity;
+
+import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("EMPL");
+		EntityManager em=emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		Employee a= new Employee();
+	
+		a.setId(105);
+		a.setName("Rajesh");
+		a.setSalary(11000);
+		em.persist(a);
+		
+	
+		
+		em.getTransaction().commit();
+		
+		
+		
+		
+		em.close();
+		emf.close();
+		
+		
+		System.out.println("Transaction Successfull");
+
+	}
+
+}
